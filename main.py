bb5c7b0c5a03954835d7dc126fe32353b54147ed
fksk94 <fksk94@gmail.com>
2021-04-04 01:55:43 +0900

    feat: 🎸 되나유?

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-some_file_path = "large-video-file.mp4"
+route = "hi2.m3u8"
 app = FastAPI()
 
 origins = ['*']
@@ -18,8 +18,24 @@ app.add_middleware(
 
 @app.get("/")
 def main():
-    file_like = open("hi.mp4", mode="rb")
-    return StreamingResponse(file_like, media_type="application/apple.vnd.mpegurl")
+    file_like = open(route, mode="rb")
+    return StreamingResponse(file_like, media_type="application/vnd.apple.mpegurl")
+
+@app.get("/a")
+def madficssfsdnh():
+    file_like = open(route, mode="rb")
+    return StreamingResponse(file_like, media_type="video/mp4")
+
+@app.get("/b")
+def madfissfsdnh():
+    file_like = open(route, mode="rb")
+    return StreamingResponse(file_like, media_type="application/x-mpegurl")
+
+@app.get("/c")
+def madfisfsfddnh():
+    file_like = open(route, mode="rb")
+    return StreamingResponse(file_like, media_type="audio/mpegurl")
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8082, reload=True)
