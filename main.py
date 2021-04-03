a0f1ea3028c1b8bbe5019717d56d15a17c0ec300
fksk94 <fksk94@gmail.com>
2021-04-04 00:30:07 +0900

    feat: 🎸 cors 처리

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 some_file_path = "large-video-file.mp4"
 app = FastAPI()
 
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def main():
